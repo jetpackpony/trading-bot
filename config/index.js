0347d8c4554e57ed35f66ac8eeead22aff52c75f
jetpackpony <jetpackpony@gmail.com>
2018-01-06 05:47:43 +0300

    Add nconf to manage config

---
+++ config/index.js
@@ -0,0 +1,26 @@
+const nconf = module.exports = require('nconf');
+const path = require('path');
+
+nconf
+  .argv()
+  .env([
+    'API_KEY',
+    'API_SECRET'
+  ])
+  .file({
+    file: path.join(
+      __dirname,
+      `${process.env.NODE_ENV || 'development'}.config.json`
+    )
+  })
+  .defaults({});
+
+// Check for required settings
+checkConfig('API_KEY');
+checkConfig('API_SECRET');
+
+function checkConfig (setting) {
+  if (!nconf.get(setting)) {
+    throw new Error(`You must set ${setting} as an environment variable or in config.json!`);
+  }
+}
