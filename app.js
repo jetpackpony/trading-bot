b721800e28642cc3c80e5f9f46d8fa7a1e94736f
jetpackpony <jetpackpony@gmail.com>
2018-01-08 14:29:00 +0300

    Rename a bunch of variables and improve output formatting

--- app.js
+++ app.js
@@ -14,23 +14,28 @@ const {
 const pairName = config.get('pairName');
 const purchasePrice = config.get('purchasePrice');
 const amount = config.get('amount');
-// take-profit-diff = ratio * stop-loss-diff
-const ratio = config.get('lossProfitRatio') || 1.5;
+// take-profit-price = profitLossRatio * stop-loss-price
+const profitLossRatio = config.get('profitLossRatio') || 1.5;
 // lower loss border in percent
-const stopLossDiff = config.get('lossDiff') || 0.075;
+const stopLoss = config.get('stopLoss') || 0.075;
 
-const stopLoss = round(purchasePrice * (1 - stopLossDiff), 8);
-const takeProfit = round(purchasePrice * (1 + stopLossDiff * ratio), 8);
+const stopLossPrice = round(purchasePrice * (1 - stopLoss), 8);
+const takeProfitPrice =
+  round(purchasePrice * (1 + stopLoss * profitLossRatio), 8);
 
-console.log(`Purchase price: ${purchasePrice.toFixed(8)}`);
-console.log(`Stop loss at:   ${stopLoss.toFixed(8)}`);
-console.log(`Take profit at: ${takeProfit.toFixed(8)}`);
+console.log(`         Watching: ${pairName}`);
+console.log(`           Amount: ${amount}`);
+console.log(`     Purchased at: ${purchasePrice.toFixed(8)}`);
+console.log(`Profit/Loss ratio: ${profitLossRatio}`);
+console.log(`        Stop loss: ${stopLoss * 100}%`);
+console.log(`     Stop loss at: ${stopLossPrice.toFixed(8)} (${(stopLossPrice / purchasePrice * 100).toFixed(2)}%)`);
+console.log(`   Take profit at: ${takeProfitPrice.toFixed(8)} (${(takeProfitPrice / purchasePrice * 100).toFixed(2)}%)`);
 eraseWrite('Connecting to the thing...');
 
 let selling = false;
 const socket = tickPrice(pairName, (price) => {
   if (!selling) {
-    if (shouldSell(price, stopLoss, takeProfit)) {
+    if (shouldSell(price, stopLossPrice, takeProfitPrice)) {
       selling = true;
       sell(pairName, amount, price);
     } else {
@@ -39,12 +44,12 @@ const socket = tickPrice(pairName, (price) => {
   }
 });
 
-const shouldSell = (price, stopLoss, takeProfit) => {
-  return (price <= stopLoss || price >= takeProfit);
+const shouldSell = (price, stopLossPrice, takeProfitPrice) => {
+  return (price <= stopLossPrice || price >= takeProfitPrice);
 };
 
 const sell = (pairName, amount, price) => {
-  eraseWrite(`Selling at:     ${price}\n`);
+  eraseWrite(`       Selling at: ${price}\n`);
   sellPair(pairName, amount, price, (err, data) => {
     if (err) {
       console.log(`Failed to sell. Err: ${err}`);
@@ -59,5 +64,6 @@ const sell = (pairName, amount, price) => {
 
 const getDots = createDotsMaker();
 const wait = (pairName, price) => {
-  eraseWrite(`Waiting at:     ${price}${getDots()}`);
+  const prc = `${price} (${(price / purchasePrice * 100).toFixed(2)}%)`
+  eraseWrite(`    Current price: ${prc}${getDots()}`);
 };
