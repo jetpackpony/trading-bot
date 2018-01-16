ff7f07e5fb5c2de157f7f19df1893f9511fd3135
jetpackpony <jetpackpony@gmail.com>
2018-01-16 21:18:55 +0300

    Update featureHistoryWindow to handle larger datasets

--- featuresHistoryWindow.js
+++ featuresHistoryWindow.js
@@ -1,7 +1,11 @@
 const R = require('ramda');
 const fs = require('fs');
-const csv = require('fast-csv');
+const readline = require('readline');
 const { round } = require('math-precision');
+const {
+  eraseWrite,
+  consoleReset
+} = require('./helpers/ansiConsole');
 let {
   convertWindow,
   willPriceJump,
@@ -26,40 +30,39 @@ const extractFeatures =
         convertWindow(windowSize,
           willPriceJump(topPercent, bottomPercent));
 
-      let writeCSVStream;
-      let writeToCSV;
-      if (writeData) {
-        writeCSVStream = csv.createWriteStream();
-        const writableStream = fs.createWriteStream(outputFile);
-        writeCSVStream.pipe(writableStream);
-        writeToCSV =
-          writeCSVStream.write.bind(writeCSVStream);
-      }
-
+      const outputStream =
+        (writeData)
+        ? fs.createWriteStream(outputFile)
+        : null;
+      const inputStream = readline.createInterface({
+        input: fs.createReadStream(inputFile)
+      });
       let curWindow = [];
       let totalExamples = 0;
       let posExamples = 0;
+      let rows = 0;
       let ys = [];
-      const readCSVSteam =
-        csv()
-        .on("data", function(line){
-          // Header line gets removed before we start to work
-          curWindow.push(line);
+      inputStream
+        .on('line', (line) => {
+          eraseWrite(`Doing row ${rows++}`);
+          curWindow = R.append(R.split(',', line), curWindow);
           if (curWindow.length > windowSize + postWindowSize) {
+            // Header line gets removed the first time we here
             curWindow = curWindow.slice(1);
             let wind = convWindow(curWindow);
             totalExamples++;
             ys.push(R.last(wind));
             (R.last(wind) === 1) ? posExamples++ : null;
             if (writeData) {
-              writeToCSV(wind);
+              outputStream.write(R.join(',', wind) + '\n');
             }
           }
         })
-        .on("end", function(){
+        .on('close', () => {
           if (writeData) {
-            writeCSVStream.end();
+            outputStream.end();
           }
+          consoleReset();
           const yWithGaps = getYWithGaps(postWindowSize, ys);
           const posWithGaps = countOnes(yWithGaps);
           const dealsPerDay =
@@ -79,8 +82,6 @@ const extractFeatures =
           };
           resolve(output);
         });
-      const readFileStream = fs.createReadStream(inputFile);
-      readFileStream.pipe(readCSVSteam);
     });
   };
 
