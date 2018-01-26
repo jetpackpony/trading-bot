159258ac0ed87657ae856e28ebb2292b531ff720
jetpackpony <jetpackpony@gmail.com>
2018-01-27 00:08:01 +0300

    Use plotly.js instead of chartsjs for charting

--- charts/renderChart.js
+++ charts/renderChart.js
@@ -5,8 +5,11 @@ const path = require('path');
 const template = fs.readFileSync(path.join(__dirname, 'chart.mustache'), 'utf-8');
 Mustache.parse(template);
 
-const render = (chartConfig, name, dir = '') => {
-  const output = Mustache.render(template, { chartConfig: JSON.stringify(chartConfig) });
+const render = (chartData, chartLayout, name, dir = '') => {
+  const output = Mustache.render(template, {
+    chartData: JSON.stringify(chartData),
+    chartLayout: JSON.stringify(chartLayout),
+  });
   const file = path.join(__dirname, dir, name + '.html');
   fs.writeFileSync(file, output);
   return path.join('file://', file);
