5ed77ddd7df21352254179175b99bc958d4dd109
jetpackpony <jetpackpony@gmail.com>
2018-02-28 15:03:49 +0300

    Add missing methods to file logger

--- loggers/file.js
+++ loggers/file.js
@@ -4,9 +4,12 @@ const makeLogger = async ({ logId }) => {
   let logFileName = `logs/dealsLog-${logId}.log`;
 
   return {
-    log: async ({ deals, actions }) => {
+    logDeals: async (deals) => {
       fs.writeFileSync(logFileName, JSON.stringify(deals, null, 2));
-    }
+    },
+    logAction: async (action) => {},
+    logDailyStats: async (stats) => {},
+    stopLogger: async () => {}
   };
 };
 module.exports = makeLogger;
